dd8f18ce5e12ad7e1a7880f45c2ae4c282d8caa1
aktarulrahul <contact.aktarul@gmail.com>
2022-12-11 00:39:05 +0600

    explored core components

--- App.tsx
+++ App.tsx
@@ -1,10 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Button,
+  Image,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import { CustomButton } from './components/Button';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <SafeAreaView>
+      <View style={{ padding: 10, backgroundColor: '#25DF', height: 100 }}>
+        <Text style={{ color: '#fff' }}>Checking</Text>
+      </View>
+      <Image
+        source={require('./assets/icon.png')}
+        resizeMode="contain"
+        style={{ height: 100, width: 100, marginTop: 10 }}
+      />
+      <Image
+        source={{ uri: 'https://picsum.photos/200/300' }}
+        resizeMode="contain"
+        style={{ height: 200, width: 100, marginTop: 10 }}
+      />
+      <Button
+        // style={styles.button}
+        title="Learn More"
+        // color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      <Pressable onPress={() => alert('Pressed!')}>
+        <Text style={styles.button}>Pressable</Text>
+      </Pressable>
+      <CustomButton
+        title="Custom 1"
+        style={{ margin: 10, textAlgin: 'center' }}
+        textStyle={{ color: '#23dfec', fontSize: 20, fontWeight: 'bold' }}
+      />
+      <CustomButton
+        title="Custom 2"
+        style={{ margin: 10, textAlgin: 'center' }}
+        textStyle={{ color: '#23dfec', fontSize: 20, fontWeight: 'bold' }}
+      />
+    </SafeAreaView>
   );
 }
 
@@ -14,5 +54,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    textAlign: 'center',
   },
 });
