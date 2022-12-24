00f6f31dd96cfe12a4a8714fcd3505a25c7aeb72
aktarulrahul <contact.aktarul@gmail.com>
2022-12-24 12:55:17 +0600

    project init

--- App.tsx
+++ App.tsx
@@ -1,21 +1,48 @@
+import { useFonts } from 'expo-font';
+import { useCallback } from 'react';
 import {
   Button,
   Image,
   Pressable,
   SafeAreaView,
   StyleSheet,
-  Text,
   View,
 } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import spacing from './src/theme/spacing';
+import { Text } from './src/components/text/Text';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+    'Spartan-Bold': require('./assets/fonts/Spartan-Bold.ttf'),
+    'Spartan-Regular': require('./assets/fonts/Spartan-Regular.ttf'),
+  });
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return (
-    <SafeAreaView>
+    <SafeAreaView onLayout={onLayoutRootView} style={styles.container}>
       <View>
-        <Text>Hi</Text>
+        <Text preset="h1">Hi</Text>
       </View>
     </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
+  },
+});
